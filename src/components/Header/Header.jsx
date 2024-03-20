000cfe6305e238dd28d7ff7409eddc607fb719bd
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-03-20 17:51:55 +0600

    load user data

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,6 +7,7 @@ const Header = () => {
             <nav>
                 <span>My Website</span>
                 <Link to="/">Home</Link>
+                <Link to="/users">Users</Link>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact us</Link>
             </nav>
