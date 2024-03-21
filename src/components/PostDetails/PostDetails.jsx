0e97b15afd9149d67aac6e633cc555b4fdc4df3c
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-03-21 19:58:51 +0600

    add loading navigation

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,12 +1,22 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
 
 
 const PostDetails = () => {
     const post = useLoaderData();
-    const {title} = post
+    const {postId} = useParams();
+    const navigate = useNavigate();
+    const { title, body } = post
+    console.log(postId);
+
+    const handleGoBack = () => {
+        navigate(-1);
+    }
+
     return (
         <div>
             <h4>Title: {title}</h4>
+            <p>{body}</p>
+            <button onClick={handleGoBack}>Go Back</button>
         </div>
     );
 };
